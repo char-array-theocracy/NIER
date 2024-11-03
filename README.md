9c25decd90a8c6560c7c0bba3a2637c62741fa5d
theocracy <lazowski578@gmail.com>
2024-11-03 17:55:59 +0200

    Basic fastcgi, lighttpd config

---
+++ README.md
@@ -0,0 +1,14 @@
+# NIER
+
+## Dependencies
+`lighttpd, apache-utils(specifically htpasswd), fcgi, fcgi-devel`
+
+## Setup
+1. Clone repository into home folder - `git clone https://github.com/char-array-theocracy/NIER`
+2. Install dependencies
+3. Create users [click here!](#user-creation)
+4. Run server using `./scripts/start_server.sh`
+## User creation
+- User data is stored in `config/.htpasswd`. 
+- **User passwords are stored as hashes!**
+- To manually create a new user you use `htpasswd -c /home/$USER/NIER/config/.htpasswd username`, it will prompt you for a password once command is run. **You might need to use elevated permissions**.
---
+++ scripts/start_server.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TEMPLATE_CONFIG="/home/$USER/NIER/config/lighttpd.conf.def"
+ACTUAL_CONFIG="/home/$USER/NIER/config/lighttpd.conf"
+
+# Replace placeholder with user name
+sed "s|0___USER___0|$USER|g" "$TEMPLATE_CONFIG" > "$ACTUAL_CONFIG"
+
+# Check if all dynamic fastcgi programs are built if not build them.
+SRC_DIR="/home/$USER/NIER/src/RASPB-NIER/quick-apps"
+DEST_DIR="/home/$USER/NIER/assets/dynamic"
+
+# Compile each .c file in the source directory
+for c_file in "$SRC_DIR"/*.c; do
+    base_name=$(basename "$c_file" .c)
+    
+    output_file="$DEST_DIR/$base_name.fcgi"
+
+    gcc "$c_file" -o "$output_file" -lfcgi
+
+    if [ $? -eq 0 ]; then
+        echo "Compiled $c_file to $output_file successfully."
+    else
+        echo "Failed to compile $c_file."
+    fi
+done
+
+# Run lighthttpd
+
+sudo lighttpd -D -f "/home/$USER/NIER/config/lighttpd.conf"
