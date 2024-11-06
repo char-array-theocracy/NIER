fe84fcfcbe6e563337d553bf28ae12b8a0526486
theocracy <lazowski578@gmail.com>
2024-11-06 17:01:42 +0200

    Added backend logging, fixed segfault

--- src/RASPB-NIER/responses.c
+++ src/RASPB-NIER/responses.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <pthread.h>
+
+pthread_mutex_t client_list_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 char *test_name_age (cJSON *json) {
-            // Extract "name" and "age" fields
             cJSON *name = cJSON_GetObjectItem(json, "name");
             cJSON *age = cJSON_GetObjectItem(json, "age");
-            int len = strlen(name->valuestring) + 23;
+            int len = strlen(name->valuestring) + 22;
             char *string = calloc(len, 1);
             if (cJSON_IsString(name) && cJSON_IsNumber(age)) {
                 snprintf(string, len, "Name: %s\nOldness: %d\n\0", name->valuestring, age->valueint);
             } else {
-                snprintf(string, len, "Error: Invalid JASON structure\n", name->valuestring, age->valueint);
+                snprintf(string, len, "Error: Invalid JSON structure\n", name->valuestring, age->valueint);
             }
             return string;
 }
